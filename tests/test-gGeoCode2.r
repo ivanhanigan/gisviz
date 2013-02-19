b8881a7f7447415e060d9f5fd886710f155da9f7
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-02-19 12:07:37 +1100

    tests

--- tests/test-gGeoCode2.r
+++ tests/test-gGeoCode2.r
@@ -1,8 +1,10 @@
 
 ################################################################
 # name:gGeoCode2
+source("../R/gGeoCode2.r")
 address <- "1 Lineaus way acton canberra"
-test_that("address is returned")
+test_that("address is returned",
 {
   expect_that(nrow(gGeoCode2(address)) == 1, is_true())
 }
+)
