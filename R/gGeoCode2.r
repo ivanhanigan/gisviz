21807ebf0bdbc115dcbdd2e4b4b5e986f1d31227
Ivan <ivan.hanigan@gmail.com>
2013-03-15 15:54:58 +1100

    gGeoCode2 now takes a vector of addresses

--- R/gGeoCode2.r
+++ R/gGeoCode2.r
@@ -11,26 +11,58 @@ gGeoCode2 <- function(str, first=T){
     sapply(getNodeSet(doc, path), function(el) xmlValue(el))
   }
   
-  
-  str <- gsub(' ','%20',str)
-  u <- sprintf('https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=%s',str)
-  xml.response <- getURL(u, ssl.verifypeer=FALSE)
-  
-  doc <- xmlTreeParse(xml.response, useInternal=TRUE, asText=TRUE)
-  
-  
-  
-  lat <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lat')
-  lng <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lng')
-  if(length(lng) == 1 & first == F){
+  if(length(str) == 1)
+  {
+    str <- gsub(' ','%20',str)
+    u <- sprintf('https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=%s',str)
+    xml.response <- getURL(u, ssl.verifypeer=FALSE)
     
-    out <- c(str, lng, lat)
-  } else if(length(lng) >= 1 & first == T) {
-    out<-c(str, lng[1], lat[1])
+    doc <- xmlTreeParse(xml.response, useInternal=TRUE, asText=TRUE)
+    
+    
+    
+    lat <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lat')
+    lng <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lng')
+    if(length(lng) == 1 & first == F){
+      
+      out <- c(str, lng, lat)
+    } else if(length(lng) >= 1 & first == T) {
+      out<-c(str, lng[1], lat[1])
+    } else {
+      out<-c(str, NA, NA)
+    }
+    out<-as.data.frame(t(out))
   } else {
-    out<-c(str, NA, NA)
+    pointTable<-as.data.frame(matrix(nrow=0, ncol=3))
+    
+    for(index in 1: length(str))
+    {
+      #index <- 1
+      str2 <- gsub(' ','%20',str[index])
+      u <- sprintf('https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=%s',str2)
+      xml.response <- getURL(u, ssl.verifypeer=FALSE)
+      
+      doc <- xmlTreeParse(xml.response, useInternal=TRUE, asText=TRUE)
+      
+      
+      
+      lat <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lat')
+      lng <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lng')
+      
+      if(length(lng) == 1 & first == F){
+        
+        out <- c(str2, lng, lat)
+      } else if(length(lng) >= 1 & first == T) {
+        out<-c(str2, lng[1], lat[1])
+      } else {
+        out<-c(str2, NA, NA)
+      }
+      pointTable<-rbind(pointTable,as.data.frame(t(out)))
+    }
+    #names(pointTable)<-c('Location', 'Latitude', 'Longitude')
+    out <- pointTable
   }
-  out<-as.data.frame(t(out))
+
   names(out) <- c('address','long','lat')
   out$long <- as.numeric(as.character(out$long))
   out$lat <- as.numeric(as.character(out$lat))
@@ -42,5 +74,7 @@ gGeoCode2 <- function(str, first=T){
 
 
 # address <- "1 Lineaus way acton canberra"
-#xy <- gGeoCode2(address)
+#address2 <- c("1 Lineaus way acton canberra", "15 follett street scullin")
+xy <- gGeoCode2(address2)
+xy
 #str(xy)
