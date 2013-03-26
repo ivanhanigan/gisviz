2025e010ed1142cbea62aaf777853dee5e86a320
ivanhanigan <ivanhanigan@gmail.com>
2013-03-27 09:47:45 +1100

    new colour scheme

--- R/plotMap.r
+++ R/plotMap.r
@@ -6,28 +6,23 @@
 # http://stackoverflow.com/questions/1260965/developing-geographic-thematic-maps-with-r             
 ################################################################################
 
-if (!require(maptools)) install.packages('maptools', repos='http://cran.csiro.au'); require(maptools)
-if (!require(RColorBrewer)) install.packages('RColorBrewer', repos='http://cran.csiro.au'); require(RColorBrewer)
-# library(maptools)
-# library(RColorBrewer)
-
-
-
-plotMap <- function(maptitle = 'map', stat=NA, region.map=NA,  cellsmap=region.map, plotdir = getwd(), probs=seq(0,1,.2), outfile = NA)
+plotMap <- function(maptitle = 'map', stat=NA, region.map=NA, brew.pal = "RdYlBu", invert.brew.pal = TRUE,  cellsmap=region.map, 
+                    plotdir = getwd(), probs=seq(0,1,.2), outfile = NA)
   {
-  level.labels = get.levels(cellsmap=cellsmap,stat=stat,probs=probs)
+  level.labels <- get.levels(cellsmap=cellsmap,stat=stat,probs=probs)
 # create a new variable in cells.map to bin the data into categories 
-  cells.map=cellsmap
-  bins = quantile(cells.map@data[,stat], probs, na.rm=T)
-  cells.map@data$bins = cut(cells.map@data[,stat], bins, include.lowest=TRUE)
+  cells.map <- cellsmap
+  bins <- quantile(cells.map@data[,stat], probs, na.rm=T)
+  cells.map@data$bins <- cut(cells.map@data[,stat], bins, include.lowest=TRUE)
 # Replace the charachter "levels" attribute with character colors
-  col.vec = brewer.pal(length(level.labels),"YlOrRd")
+  col.vec <- brewer.pal(length(level.labels),brew.pal)
+  if(invert.brew.pal == TRUE) col.vec <- col.vec[length(col.vec):1]
   levels(cells.map@data$bins) <- col.vec
 # Open a windows graphics device so that we can see what's happening 
 # windows(11.7,8.3)
 # Split the figure to leave room at the right for a legend, and room
 # at the top margin for a title   
-  par(fig = c(0,0.8,0,1), mar=c(2,2,2,0))
+  par(fig = c(0,0.7,0,1), mar=c(2,2,2,0))
 # plot the map object with no border around the rectangels, and with colors
 # dictated by new variable we created, which holds the colours as its levels
 # paramater.    
@@ -40,8 +35,8 @@ axis(2)
 axis(1)
 box()   
   plot(region.map, add=TRUE, lwd=1)
-  mtext(maptitle, side = 3, cex = 2, line = -0.5)
-  par(fig = c(0.8,1,0,1), mar=c(0,0,0,0), new = FALSE)  
+  mtext(maptitle, side = 3, cex = 2, line = 0)
+  par(fig = c(0.7,1,0,1), mar=c(0,0,0,0), new = FALSE)  
   legend("left", level.labels, fill=col.vec, bty="n", xpd=TRUE, 
         title="Legend")
 
