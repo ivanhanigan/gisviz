46a8b42c23a981de755ebf993dec31fa392598ba
ivanhanigan <ivanhanigan@gmail.com>
2013-03-29 16:03:43 +1100

    additions to plotmap to orgmode

--- R/gGeoCode2.r
+++ R/gGeoCode2.r
@@ -6,88 +6,31 @@
 gGeoCode2 <- function(str, first=T){
   if(!require(XML)) install.packages('XML'); require(XML)
   if(!require(RCurl)) install.packages('RCurl'); require(RCurl)
-  
-  getDocNodeVal <- function(doc, path){
+  getDocNodeVal=function(doc, path){
     sapply(getNodeSet(doc, path), function(el) xmlValue(el))
   }
   
-  if(!is.data.frame(str) & length(str) == 1)
-  {
-    str2 <- gsub(' ','%20',str)
-    u <- sprintf('https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=%s',str2)
-    xml.response <- getURL(u, ssl.verifypeer=FALSE)
-    
-    doc <- xmlTreeParse(xml.response, useInternal=TRUE, asText=TRUE)
-    
-    
-    
-    lat <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lat')
-    lng <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lng')
-    if(length(lng) == 1 & first == F){
-      
-      out <- c(str, lng, lat)
-    } else if(length(lng) >= 1 & first == T) {
-      out<-c(str, lng[1], lat[1])
-    } else {
-      out<-c(str, NA, NA)
-    }
-    out<-as.data.frame(t(out))
-  } else {
-    
-    if(is.data.frame(str)){
-      if(ncol(str) == 1)
-        {
-          str <- as.character(str[,1])
-        } else {
-      stop("only character vectors or singlecolumn dataframes allowed")
-      }
-    }
-    
-    pointTable<-as.data.frame(matrix(nrow=0, ncol=3))
-    for(index in 1: length(str))
-    {
-      #index <- 1
-      str2 <- gsub(' ','%20',str[index])
-
-      u <- sprintf('https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=%s',str2)
-      xml.response <- getURL(u, ssl.verifypeer=FALSE)
-      
-      doc <- xmlTreeParse(xml.response, useInternal=TRUE, asText=TRUE)
-      
-      
-      
-      lat <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lat')
-      lng <- getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lng')
-      
-      if(length(lng) == 1 & first == F){
-        
-        out <- c(str[index], lng, lat)
-      } else if(length(lng) >= 1 & first == T) {
-        out<-c(str[index], lng[1], lat[1])
-      } else {
-        out<-c(str[index], NA, NA)
-      }
-      pointTable<-rbind(pointTable,as.data.frame(t(out)))
-    }
-    #names(pointTable)<-c('Location', 'Latitude', 'Longitude')
-    out <- pointTable
-  }
-
-  names(out) <- c('address','long','lat')
-  out$long <- as.numeric(as.character(out$long))
-  out$lat <- as.numeric(as.character(out$lat))
   
+  str=gsub(' ','%20',str)
+  u=sprintf('https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=%s',str)
+  xml.response <- getURL(u, ssl.verifypeer=FALSE)
+  
+  doc = xmlTreeParse(xml.response, useInternal=TRUE, asText=TRUE)
+  
+  
+  
+  lat=getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lat')
+  lng=getDocNodeVal(doc, '/GeocodeResponse/result/geometry/location/lng')
+  if(length(lng) == 1 & first == F){
+    
+    out<-c(str, lat, lng)
+  } else if(length(lng) >= 1 & first == T) {
+    out<-c(str, lat[1], lng[1])
+  } else {
+    out<-c(str, NA, NA)
+  }
+  out<-as.data.frame(t(out))
+  names(out) <- c('address','lat','long')
   return(out)
   
 }
-
-
-
-# address <- "1 Lineaus way acton canberra"
-# address2 <- c("1 Lineaus way acton canberra", "15 follett street scullin")
-# address3 <- as.data.frame(address2)
-# xy <- gGeoCode2(address)
-# xy <- gGeoCode2(address2)
-# xy <- gGeoCode2(address3)
-# xy
-#str(xy)
--- R/plotMap.r
+++ R/plotMap.r
@@ -2,53 +2,56 @@
 ################################################################
 # name:plotMap
 ################################################################################
-# Write a mapping function, form of which was taken from here:
-# http://stackoverflow.com/questions/1260965/developing-geographic-thematic-maps-with-r             
+# A general mapping function, form of which was taken from here:
+# http://stackoverflow.com/questions/1260965/developing-geographic-thematic-maps-with-r
 ################################################################################
 
-plotMap <- function(maptitle = 'map', stat=NA, region.map=NA, brew.pal = "RdYlBu", invert.brew.pal = TRUE,  cellsmap=region.map, 
-                    plotdir = getwd(), probs=seq(0,1,.2), outfile = NA)
-  {
+plotMap <- function(maptitle = 'map', stat=NA, region.map=NA,
+                    brew.pal = "RdYlBu",
+                    invert.brew.pal = TRUE,
+                    cellsmap=region.map,
+                    plotdir = getwd(),
+                    probs=seq(0,1,.2),
+                    outfile = NA)
+{
   level.labels <- get.levels(cellsmap=cellsmap,stat=stat,probs=probs)
-# create a new variable in cells.map to bin the data into categories 
+  # create a new variable in cells.map to bin the data into categories
   cells.map <- cellsmap
   bins <- quantile(cells.map@data[,stat], probs, na.rm=T)
-  cells.map@data$bins <- cut(cells.map@data[,stat], bins, include.lowest=TRUE)
-# Replace the charachter "levels" attribute with character colors
+  cells.map@data$bins <- cut(cells.map@data[,stat],
+                             bins, include.lowest=TRUE
+                             )
+  # Replace the character "levels" attribute with character colors
   col.vec <- brewer.pal(length(level.labels),brew.pal)
   if(invert.brew.pal == TRUE) col.vec <- col.vec[length(col.vec):1]
   levels(cells.map@data$bins) <- col.vec
-# Open a windows graphics device so that we can see what's happening 
-# windows(11.7,8.3)
-# Split the figure to leave room at the right for a legend, and room
-# at the top margin for a title   
+  # Open a windows graphics device so that we can see what's happening
+  # windows(11.7,8.3)
+  # Split the figure to leave room at the right for a legend, and room
+  # at the top margin for a title
   par(fig = c(0,0.7,0,1), mar=c(2,2,2,0))
-# plot the map object with no border around the rectangels, and with colors
-# dictated by new variable we created, which holds the colours as its levels
-# paramater.    
-  plot(cells.map, 
-    border = FALSE, 
-    axes = FALSE, 
+  # plot the map object with no border around the rectangels, and with colors
+  # dictated by new variable we created, which holds the colours as its levels
+  # paramater.
+  plot(cells.map,
+    border = FALSE,
+    axes = FALSE,
     las = 1,
     col = as.character(cells.map@data$bins))
-axis(2)
-axis(1)
-box()   
+  axis(2)
+  axis(1)
+  box()
   plot(region.map, add=TRUE, lwd=1)
   mtext(maptitle, side = 3, cex = 2, line = 0)
-  par(fig = c(0.7,1,0,1), mar=c(0,0,0,0), new = FALSE)  
-  legend("left", level.labels, fill=col.vec, bty="n", xpd=TRUE, 
+  par(fig = c(0.7,1,0,1), mar=c(0,0,0,0), new = FALSE)
+  legend("left", level.labels, fill=col.vec, bty="n", xpd=TRUE,
         title="Legend")
 
-if(!is.na(outfile)){
-# # paste windows device to jpeg device
-  dev.copy(jpeg, file = paste(plotdir, '/',outfile,'.jpg', sep = ""), width = 11.75, 
-    height = 8.3, units = "in", pointsize = 12, quality = 75, bg = "white", 
-    res = 150, restoreConsole = TRUE) 
-  graphics.off()
-}
+  if(!is.na(outfile)){
+  # # paste windows device to jpeg device
+    dev.copy(jpeg, file = paste(plotdir, '/',outfile,'.jpg', sep = ""), width = 11.75,
+      height = 8.3, units = "in", pointsize = 12, quality = 75, bg = "white",
+      res = 150, restoreConsole = TRUE)
+    graphics.off()
   }
-
-  
-# get.levels(cellsmap=d,stat='DAILY_MAX_',probs=seq(0,1,.1))
-# plotMap(cellsmap=d,stat='DAILY_MAX_',region.map=grd,probs=seq(0,1,.1))
+}
