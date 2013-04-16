48afb86919b35359104e4736ec98618a384a2076
ivanhanigan <ivan.hanigan@gmail.com>
2013-04-17 07:19:29 +1000

    remodelling gGeoCode2 to take multiple addresses in a dataframe

--- tests/test-gGeoCode2.r
+++ tests/test-gGeoCode2.r
@@ -1,17 +1,28 @@
 
 ################################################################
-  # name:gGeoCode2
-#  source("../R/gGeoCode2.r")
-  require(testthat)
-  require(devtools)
-  load_all()
-  address <- "1 Lineaus way acton canberra"
-  locn <-   gGeoCode2(address)
-  str(locn)
-  test_that("address is returned",
-  {
-    expect_that(nrow(
-                     gGeoCode2(address)
-                     ) == 1, is_true())
-  }
-  )
+# name:gGeoCode2
+#source("../R/gGeoCode2.r")
+require(testthat)
+require(devtools)
+load_all()
+  
+address <- "1 Lineaus way acton canberra"
+address2 <- c("1 Lineaus way acton canberra", "15 follett street scullin")
+address3 <- as.data.frame(address2)
+xy <- gGeoCode2(address)
+xy <- gGeoCode2(address2)
+xy <- gGeoCode2(address3)
+xy
+str(xy)
+ 
+ 
+address <- "1 Lineaus way acton canberra"
+locn <-   gGeoCode2(address)
+str(locn)
+test_that("address is returned",
+{
+  expect_that(nrow(
+                   gGeoCode2(address)
+                   ) == 1, is_true())
+}
+)
