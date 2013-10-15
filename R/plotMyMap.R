9e9887d5c60232ddee1c69dff8334676c01a6270
ivanhanigan <ivan.hanigan@gmail.com>
2013-10-15 11:03:24 +1100

    make map take matrix of latlongs

--- R/plotMyMap.R
+++ R/plotMyMap.R
@@ -12,7 +12,7 @@ plotMyMap <- function(location, xl = c(-180,180), yl = c(-50,50), googlemaps = F
   if(googlemaps == T){
     points(geocode(location), pch =16, col = 'red')
   } else {
-    points(location[1,], pch =16, col = 'red')
+    points(location, pch =16, col = 'red')
   }
   
 }
